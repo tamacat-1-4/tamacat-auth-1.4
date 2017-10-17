8a79c029c7870db02350c8057196e2fa08f37a28
tamacat <tamacat@f3cb667f-c9e3-46e3-bc2c-302c0b7402fb>
2017-10-17 12:38:16 +0000

    add method
    git-svn-id: https://svn.code.sf.net/p/tamacat/code/branches/tamacat-auth-1.4@1159 f3cb667f-c9e3-46e3-bc2c-302c0b7402fb

--- src/main/java/org/tamacat/auth/model/UserProfileUtils.java
+++ src/main/java/org/tamacat/auth/model/UserProfileUtils.java
@@ -1,13 +1,15 @@
 package org.tamacat.auth.model;
 
 import java.io.ByteArrayInputStream;
-import java.util.Set;
+import java.util.Collection;
 
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 
 import org.tamacat.auth.util.EncryptSessionUtils;
+import org.tamacat.dao.meta.Column;
+import org.tamacat.dao.orm.MapBasedORMappingBean;
 import org.tamacat.log.Log;
 import org.tamacat.log.LogFactory;
 import org.tamacat.util.StringUtils;
@@ -16,8 +18,9 @@ public class UserProfileUtils {
 
 	static final Log LOG = LogFactory.getLog(UserProfileUtils.class);
 	
-	public static LoginUser getProfile(String profile, LoginUser loginUser, Set<String> columns) {
+	public static <T extends LoginUser> T getProfile(String profile, T loginUser, Collection<String> columns) {
 		String json = EncryptSessionUtils.decryptSession(profile);
+		if (json == null) return null;
 		try {
 			JsonReader reader = Json.createReader(new ByteArrayInputStream(json.getBytes("UTF-8")));
 			JsonObject obj = reader.readObject();
@@ -32,9 +35,26 @@ public class UserProfileUtils {
 			LOG.trace("json="+json);
 			LOG.warn("ERROR:"+e.getMessage());
 		}
-		if (loginUser instanceof LoginUser) {
-			return (LoginUser)loginUser;
+		return loginUser;
+	}
+	
+	public static <T extends MapBasedORMappingBean<T>> T getProfile(String profile, T loginUser, Collection<Column> columns) {
+		String json = EncryptSessionUtils.decryptSession(profile);
+		if (json == null) return null;
+		try {
+			JsonReader reader = Json.createReader(new ByteArrayInputStream(json.getBytes("UTF-8")));
+			JsonObject obj = reader.readObject();
+			for (Column col : columns) {
+				String value = obj.getString(col.getName(), "");
+				if (StringUtils.isNotEmpty(value)) {
+					loginUser.val(col, value);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			LOG.trace("json="+json);
+			LOG.warn("ERROR:"+e.getMessage());
 		}
-		return null;
+		return loginUser;
 	}
 }
